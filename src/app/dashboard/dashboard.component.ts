0224c6bd47dfddd1b570b9c7f847a8bf0d687ece
ayaya <ayaya6436@gmail.com>
2023-11-02 01:23:58 +0000

    dashboard v1.2

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  map: any;
 
+  ngOnInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map);
+
+    L.marker([51.5, -0.09]).addTo(this.map)
+      .bindPopup('A sample location.')
+      .openPopup();
+  }
 }
